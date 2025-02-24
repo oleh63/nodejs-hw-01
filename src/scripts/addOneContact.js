23489a5feee15bfeb0f9c93bc47ea81671d8421e
Oleh Skrypets <olehskrupets@gmail.com>
2025-02-24 23:02:04 +0200

    add all hw-01

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,12 @@
-export const addOneContact = async () => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const addOneContact = async () => {
+  const contacts = await readContacts();
+  const newContacts = createFakeContact();
+  await writeContacts([...contacts, newContacts]);
+  return console.log('hello');
+};
 
 addOneContact();
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,13 @@
+import * as fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  try {
+    await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts, null, 2), {
+      encoding: 'utf-8',
+    });
+    console.log('Contacts update!');
+  } catch (error) {
+    console.error('error', error);
+  }
+};
